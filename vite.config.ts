dfb8d1fbebf12330f89fb211972042fb71d0968c
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2023-06-28 19:44:20 +0800

    变更 base 目录为相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue(), svgLoader(), wasm(), topLevelAwait()],
+  base: './',
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	// prevent vite from obscuring rust errors
