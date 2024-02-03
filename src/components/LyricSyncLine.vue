8dd072c5a6fd1b8089ac1548f10ed7b1c8da09ce
SteveXMH <39523898+Steve-xmh@users.noreply.github.com>
2024-02-03 23:31:37 +0800

    增加歌词行时间轴显示

--- src/components/LyricSyncLine.vue
+++ src/components/LyricSyncLine.vue
@@ -10,18 +10,19 @@
   -->
 
 <template>
-    <div :class="{
+	<div ref="itemRef" :class="{
         'lyric-line-item': true,
         'lyric-line-item-selected': currentWord.lineIndex === props.line.id,
     }" @click="
     currentWord.lineIndex = props.line.id;
 currentWord.wordIndex = 0;
 currentWord.emptyBeat = 0;
-" ref="itemRef">
-        <div class="lyric-line-item-inner">
-            <div>{{ toTimestamp(line.words?.[0]?.startTime ?? 0) }}</div>
-            <div>
-                <div :class="{
+">
+		<div class="lyric-line-item-inner">
+			<div>{{ toTimestamp(line.startTime ?? 0) }}</div>
+			<div v-if="line.words.length > 0" class="word-time">{{ toTimestamp(line.words[0].startTime ?? 0) }}</div>
+			<div class="line">
+				<div :class="{
                     'hot-line':
                         line.words.length > 0 &&
                         line.words[0].startTime <= currentTime &&
@@ -35,15 +36,20 @@ currentWord.emptyBeat = 0;
                             word.startTime <= currentTime && word.endTime > currentTime,
                         'lyric-line-word-warn': word.endTime - word.startTime < 0,
                     }">{{ word.word }}</span>
-                </div>
-                <div v-if="settings.showTranslateLine">{{ line.translatedLyric }}</div>
-                <div v-if="settings.showRomanLine">{{ line.romanLyric }}</div>
-            </div>
-        </div>
-    </div>
+				</div>
+				<div v-if="settings.showTranslateLine">{{ line.translatedLyric }}</div>
+				<div v-if="settings.showRomanLine">{{ line.romanLyric }}</div>
+			</div>
+			<div v-if="line.words.length > 0" class="word-time">{{
+					toTimestamp(line.words[line.words.length - 1].endTime ?? 0)
+				}}
+			</div>
+			<div>{{ toTimestamp(line.endTime ?? 0) }}</div>
+		</div>
+	</div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import {useAudio, useCurrentSyncWord, useSettings,} from "../store";
 import {storeToRefs} from "pinia";
 import {nextTick, ref, watch} from "vue";
@@ -51,67 +57,80 @@ import type {LyricLineWithId} from "../store/lyric";
 
 const itemRef = ref<HTMLDivElement>();
 
-const { currentTime } = storeToRefs(useAudio());
+const {currentTime} = storeToRefs(useAudio());
 const currentWord = useCurrentSyncWord();
 const settings = useSettings();
 
 const props = defineProps<{
-    line: LyricLineWithId;
+	line: LyricLineWithId;
 }>();
 
 watch(() => [currentWord.lineIndex, props.line.id], () => {
-    if (currentWord.lineIndex === props.line.id) {
-        nextTick(() => {
-            itemRef.value?.scrollIntoView({
-                behavior: "smooth",
-                block: "center",
-                inline: "center"
-            });
-        })
-    }
-}, { flush: "post" })
+	if (currentWord.lineIndex === props.line.id) {
+		nextTick(() => {
+			itemRef.value?.scrollIntoView({
+				behavior: "smooth",
+				block: "center",
+				inline: "center"
+			});
+		})
+	}
+}, {flush: "post"})
 
 function toTimestamp(duration: number) {
-    const isRemainTime = duration < 0;
+	const isRemainTime = duration < 0;
 
-    const d = Math.abs(duration / 1000);
-    const sec = d % 60;
-    const min = Math.floor((d - sec) / 60);
-    const secFixed = sec.toFixed(3);
-    const secText = "0".repeat(6 - secFixed.length) + secFixed;
+	const d = Math.abs(duration / 1000);
+	const sec = d % 60;
+	const min = Math.floor((d - sec) / 60);
+	const secFixed = sec.toFixed(3);
+	const secText = "0".repeat(6 - secFixed.length) + secFixed;
 
-    return `${isRemainTime ? "-" : ""}${min}:${secText}`;
+	return `${isRemainTime ? "-" : ""}${min}:${secText}`;
 }
 </script>
 
 <style lang="sass">
 .sync-line > *
-    margin-right: 6px
+	margin-right: 6px
+
 .lyric-line-word-warn
-    color: #EE4444
+	color: #EE4444
+
 .lyric-line-item
-    cursor: pointer
-    outline-offset: -4px
-    padding: 4px
-    border-bottom: 1px solid rgba(170, 170, 170, 0.2666666667)
-    &.lyric-line-item-selected
-        outline: 3px solid var(--att-theme-color) //#63e2b7
-    &:hover
-        background: var(--att-color-hover)
-        color: var(--att-text-color-hover)
+	cursor: pointer
+	outline-offset: -4px
+	padding: 4px
+	border-bottom: 1px solid rgba(170, 170, 170, 0.2666666667)
+
+	&.lyric-line-item-selected
+		outline: 3px solid var(--att-theme-color)
+	//#63e2b7
+	&:hover
+		background: var(--att-color-hover)
+		color: var(--att-text-color-hover)
+
 .hot-line
-    color: var(--att-theme-color-hover)
-    opacity: 0.7
+	color: var(--att-theme-color-hover)
+	opacity: 0.7
+
 .hot-word
-    color: var(--att-theme-color-pressed)
+	color: var(--att-theme-color-pressed)
+
 .current-word
-    color: var(--att-theme-color)
-    font-weight: bold
+	color: var(--att-theme-color)
+	font-weight: bold
+
 .lyric-line-item-inner
-    display: flex
-    align-items: center
-    margin: 0 12px
-    gap: 12px
-    > *:nth-child(2)
-        font-size: 18px
+	display: flex
+	align-items: center
+	margin: 0 12px
+	gap: 12px
+
+	.word-time
+		opacity: 0.75
+
+	.line
+		font-size: 18px
+		flex: 1
 </style>
