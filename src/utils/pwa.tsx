5d8f50612a0b2ecad96e5ab767193ec9c9e8a15f
apoint123 <108002475+apoint123@users.noreply.github.com>
2025-12-02 17:11:35 +0800

    fix: 在tauri环境下禁用pwa

--- src/utils/pwa.tsx
+++ src/utils/pwa.tsx
@@ -3,27 +3,29 @@ import { Button, Flex } from "@radix-ui/themes";
 import { t } from "i18next";
 import { toast } from "react-toastify";
 
-const refresh = registerSW({
-	onOfflineReady() {
-		toast.info(
-			t("pwa.offlineReady", "网站已成功离线缓存，后续可离线访问本网页"),
-		);
-	},
-	onNeedRefresh() {
-		toast.info(
-			<Flex direction="column" gap="2" align="stretch">
-				<div>
-					{t("pwa.updateRefresh", "网站已更新，刷新网页以使用最新版本！")}
-				</div>
-				<Button
-					size="2"
-					onClick={() => {
-						refresh(true);
-					}}
-				>
-					{t("pwa.refresh", "刷新")}
-				</Button>
-			</Flex>,
-		);
-	},
-});
+if (!import.meta.env.TAURI_ENV_PLATFORM) {
+	const refresh = registerSW({
+		onOfflineReady() {
+			toast.info(
+				t("pwa.offlineReady", "网站已成功离线缓存，后续可离线访问本网页"),
+			);
+		},
+		onNeedRefresh() {
+			toast.info(
+				<Flex direction="column" gap="2" align="stretch">
+					<div>
+						{t("pwa.updateRefresh", "网站已更新，刷新网页以使用最新版本！")}
+					</div>
+					<Button
+						size="2"
+						onClick={() => {
+							refresh(true);
+						}}
+					>
+						{t("pwa.refresh", "刷新")}
+					</Button>
+				</Flex>,
+			);
+		},
+	});
+}
